8c5b819a21a19dc84abb53baadbd9110ec470220
Xurshid97 <xurshedrabo@gmail.com>
2023-07-03 19:11:27 +0200

    regex test method

--- regex1.js
+++ regex1.js
@@ -1,1 +1,15 @@
-// Test method
+// Regular expression (RegEx) = Doimiy ifodalar
+// Biz ma'lum bir matnni string ichida mavjud ekanligini topmoqchi bo'lsak regex dan foydalanamiz 
+
+// 1 method === test() ==
+/* test methodi qidirilishi kerak bo'lgan qiymatni oladi va uni stringni ichida mavjud yoki yo'qligini tekshirib Xato yoki to'g'ri qiymat qaytaradi */
+// Masalan:
+
+// oddiyMatn o'zgaruvchisi yaratilib unga Assalomu aleykum qiymati berildi
+let oddiyMatn = 'Assalomu aleykum'
+
+// testRegEx o'zgaruvchisi yaratilib unga ale qiyamti berildi
+let testRegEx = /ale/
+
+// consolda true qiymat chiqdi, chunki /ale/ qiymatimiz matnning ichida mavjud va biz uni test metodi orqali tekshirib ko'rdik
+console.log(testRegEx.test(oddiyMatn));
